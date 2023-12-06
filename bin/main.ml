3e623b5b78f71e811ba823786e725c03be077090
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2023-12-07 03:05:40 +0900

    Solve problem A

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,5 @@
-let () = print_endline "Hello, World!"
+open Icpc_domestic2021
+
+let () =
+  (* NOTE: `Sys.argv` must contain a command name at the first. *)
+  match Sys.argv.(1) with "a" -> A.solve () | _ -> ()
---
+++ lib/a.ml
@@ -0,0 +1,17 @@
+exception InvalidInput
+
+(* NOTE: Assume a ≥ b *)
+let rec gcd a b = if b = 0 then a else gcd b (Int.rem a b)
+
+let solve () =
+  Util.read_input "0 0 0 0"
+  |> List.iter (fun line ->
+         let ans =
+           match line |> String.split_on_char ' ' |> List.map int_of_string with
+           | x :: xs when List.length xs = 3 ->
+               List.fold_left
+                 (fun acc y -> if acc <= y then gcd y acc else gcd acc y)
+                 x xs
+           | _ -> raise InvalidInput
+         in
+         Printf.printf "%d\n" ans)
