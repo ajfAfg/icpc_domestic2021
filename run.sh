271dff7b56112e05f4c49c87e24dee1cb4362294
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2023-12-07 21:04:03 +0900

    Update to allow testing of problems other than A

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-for problem in "data/A"?; do
-    time diff ${problem}.ans <(dune exec icpc_domestic2021 a <${problem})
+entry=$(echo "$1" | tr A-Z a-z)
+
+if [ ${#entry} -ne 1 ]; then
+    echo 'Error: Enter a problem entry.'
+    exit 1
+fi
+
+for problem in "data/$(echo $entry | tr a-z A-Z)"?; do
+    time diff ${problem}.ans <(dune exec icpc_domestic2021 $entry <${problem})
 done
