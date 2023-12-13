a1bd15e7db2ff70ee09d4680b0a74d1ad5c4bbb8
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2023-12-13 11:26:36 +0900

    Include the labels in the vertices

--- lib/c.ml
+++ lib/c.ml
@@ -80,7 +80,7 @@ end
  * Graph
  *********************)
 module G = Graph.Persistent.Graph.Concrete (struct
-  type t = Id.t
+  type t = Id.t * (int, Syntax.op) Either.t
 
   let compare = compare
   let hash = Hashtbl.hash
@@ -88,68 +88,61 @@ module G = Graph.Persistent.Graph.Concrete (struct
 end)
 
 (*********************
- * Map
- *********************)
-module IdMap = Map.Make (Id)
-
-(*********************
  * Main
  *********************)
-let graph_labels_of_exp =
-  let rec graph_labels_of_exp' graph labels id = function
-    | Syntax.ILit i ->
-        let graph = G.add_vertex graph id in
-        let labels = IdMap.add id (Either.Left i) labels in
-        (graph, labels)
-    | Op (op, exps) ->
-        let id_exp_list = List.map (fun exp -> (Id.create (), exp)) exps in
-        let graph, labels =
-          List.fold_left
-            (fun (graph', labels') (id, exp) ->
-              graph_labels_of_exp' graph' labels' id exp)
-            (graph, labels) id_exp_list
-        in
-        let graph = G.add_vertex graph id in
-        let graph =
-          List.fold_left (fun graph' id' -> G.add_edge graph' id id') graph
-          @@ List.map fst id_exp_list
-        in
-        let labels = IdMap.add id (Either.Right op) labels in
-        (graph, labels)
-  in
-  graph_labels_of_exp' G.empty IdMap.empty @@ Id.create ()
+type tree = T of (Id.t * (int, Syntax.op) Either.t) * tree list
 
-let rec traverse memo parent_opt graph labels id =
+let rec tree_of_exp = function
+  | Syntax.ILit i -> T ((Id.create (), Either.Left i), [])
+  | Op (op, exps) ->
+      T ((Id.create (), Either.Right op), List.map tree_of_exp exps)
+
+let graph_of_tree =
+  let rec graph_of_tree' graph = function
+    | T (vertex, trees) ->
+        let graph = G.add_vertex graph vertex in
+        let graph =
+          List.fold_left
+            (fun graph' tree -> graph_of_tree' graph' tree)
+            graph trees
+        in
+        List.fold_left
+          (fun graph' (T (vertex', _)) -> G.add_edge graph' vertex vertex')
+          graph trees
+  in
+  graph_of_tree' G.empty
+
+let rec traverse memo parent_opt graph vertex =
   match parent_opt with
-  | None -> traverse' memo parent_opt graph labels id
+  | None -> traverse' memo parent_opt graph vertex
   | Some parent ->
-      let key = (parent, id) in
+      let key = (parent, vertex) in
       let v =
         match Hashtbl.find_opt memo key with
-        | None -> traverse' memo parent_opt graph labels id
+        | None -> traverse' memo parent_opt graph vertex
         | Some v -> v
       in
       Hashtbl.add memo key v;
       v
 
-and traverse' memo parent_opt graph labels id =
+and traverse' memo parent_opt graph vertex =
   let children =
     match parent_opt with
-    | None -> G.succ graph id
-    | Some parent -> G.succ graph id |> List.filter (( <> ) parent)
+    | None -> G.succ graph vertex
+    | Some parent -> G.succ graph vertex |> List.filter (( <> ) parent)
   in
-  match IdMap.find id labels with
-  | Either.Left i -> (i, i)
-  | Right Syntax.Plus ->
+  match vertex with
+  | _, Either.Left i -> (i, i)
+  | _, Right Syntax.Plus ->
       children
-      |> List.map (fun child -> traverse memo (Some id) graph labels child)
+      |> List.map (fun child -> traverse memo (Some vertex) graph child)
       |> List.fold_left
            (fun (max1, min1) (max2, min2) -> (max1 + max2, min1 + min2))
            (0, 0)
-  | Right Minus ->
+  | _, Right Minus ->
       let max_min_list =
         children
-        |> List.map (fun child -> traverse memo (Some id) graph labels child)
+        |> List.map (fun child -> traverse memo (Some vertex) graph child)
       in
       let max =
         max_min_list
@@ -170,16 +163,14 @@ and traverse' memo parent_opt graph labels id =
 let solve () =
   Util.read_input "-1"
   |> List.iter (fun dataset ->
-         let graph, labels =
-           dataset |> Parser.parse_exp |> graph_labels_of_exp
+         let graph =
+           dataset |> Parser.parse_exp |> tree_of_exp |> graph_of_tree
          in
-         let memo = Hashtbl.create @@ (2 * IdMap.cardinal labels) in
+         let memo = Hashtbl.create @@ (2 * G.nb_vertex graph) in
          G.fold_vertex
-           (fun id max ->
-             match IdMap.find id labels with
-             | Either.Left _ -> max
-             | Right _ ->
-                 let max_xy x y = if x < y then y else x in
-                 max_xy max @@ fst @@ traverse memo None graph labels id)
+           (fun vertex max' ->
+             match vertex with
+             | _, Either.Left _ -> max'
+             | _, Right _ -> max max' @@ fst @@ traverse memo None graph vertex)
            graph Int.min_int
          |> Printf.printf "%d\n")
